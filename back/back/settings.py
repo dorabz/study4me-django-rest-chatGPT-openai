51dce053f257cdf3ec51189f0d9cd8dc266ba159
dorabz <72765036+dorabz@users.noreply.github.com>
2022-11-30 01:14:33 +0100

    added corsheaders

--- back/back/settings.py
+++ back/back/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+#CORS_ALLOWED_ORIGINS = [
+#   'http://localhost:3030',
+#]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'back.urls'
 
